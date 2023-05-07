5a838d8e3c4591def1dfdd93cc45c9646f9e0371
mackay <johnmackay150@gmail.com>
2023-05-07 22:55:45 +0900

    CHANGE:install.sh path pathname

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ INSTALL_DIR=$HOME/test
 if [ ! -d ${INSTALL_DIR} ]; then
     REPO_URL="https://github.com/Hiro-mackay/dotfiles/archive/main.tar.gz"
     mkdir -p ${INSTALL_DIR}
-    curl -L REPO_URL | tar xz --strip 1 -C $INSTALL_DIR
-    $INSTALL_DIR/env.sh
+    curl -L ${REPO_URL} | tar xz --strip 1 -C ${INSTALL_DIR}
+    ${INSTALL_DIR}/env.sh
 else
     echo "dotfiles already exists"
     exit 1

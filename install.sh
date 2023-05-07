83cc2832da61c097e9d460cf4ff632571a1eb9f5
mackay <johnmackay150@gmail.com>
2023-05-07 22:42:44 +0900

    ADD:test install tar.gz

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
-echo "Success run script"
-source env.sh
+#!/bin/sh
+
+INSTALL_DIR="${INSTALL_DIR:-$HOME/test}"
+
+if [ ! -d ${DOT_DIR} ]; then
+    REPO_URL="https://github.com/Hiro-mackay/dotfiles/archive/main.tar.gz"
+    mkdir -p ${INSTALL_DIR}
+    curl -L REPO_URL | tar xz --strip 1 -C $INSTALL_DIR
+    $INSTALL_DIR/env.sh
+else
+    echo "dotfiles already exists"
+    exit 1
+fi

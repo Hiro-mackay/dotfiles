6cfb2b969faebde1ca4c88ae7cbde01e6874a26f
mackay <johnmackay150@gmail.com>
2023-05-07 22:46:13 +0900

    CHANGE:INSTALL_DIR

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 INSTALL_DIR="${INSTALL_DIR:-$HOME/test}"
 
-if [ ! -d ${DOT_DIR} ]; then
+if [ ! -d ${INSTALL_DIR} ]; then
     REPO_URL="https://github.com/Hiro-mackay/dotfiles/archive/main.tar.gz"
     mkdir -p ${INSTALL_DIR}
     curl -L REPO_URL | tar xz --strip 1 -C $INSTALL_DIR
